8d4bdd5c5ff46c9f19dd22958306f205cabcc5d5
Artur Bien <artur.bien@gmail.com>
2019-04-24 19:07:14 +0200

    closes #17

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -1,20 +1,44 @@
 import React from "react";
 import propTypes from "prop-types";
 import styled from "styled-components";
+import { blockSizes } from "../common/system";
 
 const StyledDivider = styled.hr`
-  width: 100%;
-  border-bottom: 2px solid ${({ theme }) => theme.borderLightest};
-  border-top: 2px solid ${({ theme }) => theme.borderDark};
-  margin: 0;
+  ${({ vertical, theme, size }) =>
+    vertical
+      ? `
+    height: ${blockSizes[size]};
+    border-left: 2px solid ${theme.borderDark};
+    border-right: 2px solid ${theme.borderLightest};
+    margin: 0;
+    `
+      : `
+    width: 100%;
+    border-bottom: 2px solid ${theme.borderLightest};
+    border-top: 2px solid ${theme.borderDark};
+    margin: 0;
+    `}
 `;
 
-const Divider = ({ className, style, ...otherProps }) => {
-  return <StyledDivider className={className} style={style} {...otherProps} />;
+const Divider = ({ vertical, className, style, ...otherProps }) => {
+  return (
+    <StyledDivider
+      vertical={vertical}
+      as={vertical ? "div" : "hr"}
+      className={className}
+      style={style}
+      {...otherProps}
+    />
+  );
 };
 
-Divider.defaultProps = {};
+Divider.defaultProps = {
+  size: "md",
+  vertical: false
+};
 Divider.propTypes = {
+  vertical: propTypes.bool,
+  size: propTypes.oneOf(["sm", "md", "lg"]),
   className: propTypes.string,
   style: propTypes.object
 };
