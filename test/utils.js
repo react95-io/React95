526dc801448e62167f5da2ee94352a2c10f4851c
Baldi <baldilp@gmail.com>
2019-07-10 21:40:21 -0300

    Create render with theme wrapper and add tests for Avatar

---
+++ test/utils.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { render } from '@testing-library/react'
+
+import themes from '../src/components/common/themes'
+
+export const theme = themes.default
+
+export const renderWithTheme = (component) => render(
+  <ThemeProvider theme={theme}>
+    {component}
+  </ThemeProvider>
+)
