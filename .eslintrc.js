b7ef97308bb5358ec7481c39fbfa0babbea33b44
Baldi <baldilp@gmail.com>
2019-07-29 22:53:00 -0300

    Configure pre-commit hook with husky and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "airbnb"
-};
+  extends: "airbnb",
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  rules: {
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "jsx-a11y/label-has-associated-control": [ "error", {
+      "required": {
+        "some": [ "nesting", "id"  ]
+      }
+    }],
+    "jsx-a11y/label-has-for": [ "error", {
+      "required": {
+        "some": [ "nesting", "id"  ]
+      }
+    }]
+  }
+}
