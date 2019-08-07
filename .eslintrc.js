d39fd818e79e228e008de8c65cdd37a100e1edaa
Baldi <baldilp@gmail.com>
2019-08-06 21:48:19 -0300

    Add and configure prettier with pre-commit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
-  extends: "airbnb",
-  parser: "babel-eslint",
+  extends: ['airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
   env: {
     browser: true,
     es6: true,
     jest: true
   },
   rules: {
-    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "jsx-a11y/label-has-associated-control": [ "error", {
-      "required": {
-        "some": [ "nesting", "id"  ]
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id']
+        }
       }
-    }],
-    "jsx-a11y/label-has-associated-control": ["error", { assert: "either" } ],
-    "jsx-a11y/label-has-for": 'off'
+    ],
+    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+    'jsx-a11y/label-has-for': 'off',
+    'prettier/prettier': 'error'
   }
-}
+};
