997fdfd293aeba8cf56e2449763ef05274ffb3c0
Artur Bien <artur.bien@tivix.com>
2020-01-26 18:01:22 +0100

    added first tests

---
+++ src/components/Checkbox/Checkbox.spec.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { renderWithTheme } from '../../../test/utils';
+import Checkbox from './Checkbox';
+
+describe('<Checkbox />', () => {
+  describe('onChange', () => {
+    it('should call onChange when uncontrolled', () => {
+      const handleChange = jest.fn(event => event.target.checked);
+      const { getByRole } = renderWithTheme(
+        <Checkbox onChange={handleChange} />
+      );
+
+      getByRole('checkbox').click();
+
+      expect(handleChange).toHaveBeenCalledTimes(1);
+      // event.target.check is true
+      expect(handleChange.mock.results[0].value).toBe(true);
+    });
+
+    it('should call onChange when controlled', () => {
+      const checked = true;
+      const handleChange = jest.fn(event => event.target.checked);
+
+      const { getByRole } = renderWithTheme(
+        <Checkbox onChange={handleChange} checked={checked} />
+      );
+
+      getByRole('checkbox').click();
+
+      expect(handleChange).toHaveBeenCalledTimes(1);
+
+      expect(handleChange.mock.results[0].value).toBe(!checked);
+    });
+  });
+
+  describe('controlled', () => {
+    it('should check the checkbox', () => {
+      const { getByRole, rerender } = renderWithTheme(
+        <Checkbox checked={false} />
+      );
+
+      rerender(<Checkbox checked />);
+      const checkbox = getByRole('checkbox');
+
+      expect(checkbox.checked).toBe(true);
+      expect(getByRole('checkbox')).toHaveAttribute('checked');
+      //   check if proper icon was rendered
+    });
+
+    it('should uncheck the checkbox', () => {
+      const { getByRole, rerender } = renderWithTheme(<Checkbox checked />);
+      rerender(<Checkbox checked={false} />);
+      const checkbox = getByRole('checkbox');
+
+      expect(checkbox.checked).toBe(false);
+      expect(getByRole('checkbox')).not.toHaveAttribute('checked');
+
+      //   check if proper icon was rendered
+    });
+  });
+
+  describe('uncontrolled', () => {
+    it('can change checked state uncontrolled starting from defaultChecked', () => {
+      const { getByRole } = renderWithTheme(<Checkbox defaultChecked />);
+      const checkbox = getByRole('checkbox');
+
+      expect(checkbox.checked).toBe(true);
+      //   expect(getByTestId('checked-icon')).to.be.ok;
+
+      checkbox.click();
+
+      expect(checkbox.checked).toBe(false);
+      //   expect(getByTestId('unchecked-icon')).to.be.ok;
+
+      checkbox.click();
+
+      expect(checkbox.checked).toBe(true);
+      //   expect(getByTestId('checked-icon')).to.be.ok;
+    });
+  });
+});
