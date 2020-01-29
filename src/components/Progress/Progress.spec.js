1fc403803f0a87192e5667c2efc253aa9333b005
Artur Bien <artur.bien@tivix.com>
2020-01-29 19:35:17 +0100

    updated progress tests

--- src/components/Progress/Progress.spec.js
+++ src/components/Progress/Progress.spec.js
@@ -14,32 +14,19 @@ describe('<Progress />', () => {
     expect(progress).toHaveAttribute('aria-valuenow', value.toString());
   });
 
-  describe('prop: variant', () => {
-    it('"default" variant displays current percentage value', () => {
-      const value = 32;
-      const { queryByTestId } = renderWithTheme(<Progress value={value} />);
+  it('displays current percentage value', () => {
+    const value = 32;
+    const { queryByTestId } = renderWithTheme(<Progress value={value} />);
 
-      expect(queryByTestId('defaultProgress1').textContent).toBe(`${value}%`);
-      expect(queryByTestId('defaultProgress2').textContent).toBe(`${value}%`);
+    expect(queryByTestId('defaultProgress1').textContent).toBe(`${value}%`);
+    expect(queryByTestId('defaultProgress2').textContent).toBe(`${value}%`);
 
-      expect(queryByTestId('defaultProgress2')).toHaveStyleRule(
-        'clip-path',
-        `polygon( 0 0,${value}% 0,${value}% 100%,0 100% )`
-      );
+    expect(queryByTestId('defaultProgress2')).toHaveStyleRule(
+      'clip-path',
+      `polygon( 0 0,${value}% 0,${value}% 100%,0 100% )`
+    );
 
-      expect(queryByTestId('indeterminateProgress')).not.toBeInTheDocument();
-    });
-
-    it('"indeterminate" variant displays indeterminate progress', () => {
-      const { queryByTestId } = renderWithTheme(
-        <Progress variant='indeterminate' />
-      );
-
-      expect(queryByTestId('defaultProgress1')).not.toBeInTheDocument();
-      expect(queryByTestId('defaultProgress2')).not.toBeInTheDocument();
-
-      expect(queryByTestId('indeterminateProgress')).toBeInTheDocument();
-    });
+    expect(queryByTestId('indeterminateProgress')).not.toBeInTheDocument();
   });
 
   describe('prop: hideValue', () => {
