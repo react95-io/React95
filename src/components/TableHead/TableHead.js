0c8f37286f4e674ecaff1374ee5f54c48c85d735
Artur Bień <artur.bien@gmail.com>
2020-02-07 23:54:50 +0100

    remove obsolete props from TableHead

--- src/components/TableHead/TableHead.js
+++ src/components/TableHead/TableHead.js
@@ -5,22 +5,16 @@ import styled from 'styled-components';
 const StyledTableHead = styled.thead`
   display: table-header-group;
 `;
-const TableHead = ({ className, children, style, ...otherProps }) => (
-  <StyledTableHead className={className} style={style} {...otherProps}>
-    {children}
-  </StyledTableHead>
+const TableHead = ({ children, ...otherProps }) => (
+  <StyledTableHead {...otherProps}>{children}</StyledTableHead>
 );
 
 TableHead.defaultProps = {
-  children: null,
-  className: '',
-  style: {}
+  children: null
 };
 
 TableHead.propTypes = {
-  children: propTypes.node,
-  className: propTypes.string,
-  style: propTypes.shape([propTypes.string, propTypes.number])
+  children: propTypes.node
 };
 
 export default TableHead;
