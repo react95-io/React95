890d9f287fffb0105479e4cd1aafc9368218873d
Artur Bień <artur.bien@gmail.com>
2020-02-07 23:53:05 +0100

    remove obsolete props, restyle TableHeadCell

--- src/components/TableHeadCell/TableHeadCell.js
+++ src/components/TableHeadCell/TableHeadCell.js
@@ -5,54 +5,57 @@ import styled from 'styled-components';
 import { createBorderStyles } from '../common';
 import { padding } from '../common/system';
 
-// ⭕⭕⭕⭕ move text down on Click
+// TODO move text down on Click
 
 const StyledHeadCell = styled.th`
-  ${createBorderStyles()}
+  position: relative;
   padding: 0 ${padding.sm};
   display: table-cell;
   vertical-align: inherit;
   background: ${({ theme }) => theme.material};
-  &:active {
-    ${createBorderStyles({ invert: true })}
+  &:before {
+    box-sizing: border-box;
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    ${createBorderStyles()}
+
     border-left: none;
     border-top: none;
   }
-  border-left: none;
-  border-top: none;
+  &:active {
+    &:before {
+      ${createBorderStyles({ invert: true, windowBorders: true })}
+      border-left: none;
+      border-top: none;
+      padding-top: 2px;
+    }
+  }
   cursor: default;
+  &:hover {
+    color: ${({ theme }) => theme.text};
+  }
+  color: ${({ theme }) => theme.text};
+  user-select: none;
 `;
 
-const TableHeadCell = ({
-  className,
-  children,
-  style,
-  onClick,
-  ...otherProps
-}) => (
-  <StyledHeadCell
-    className={className}
-    style={style}
-    onClick={onClick}
-    onTouchStart={() => ''}
-    {...otherProps}
-  >
+const TableHeadCell = ({ children, onClick, ...otherProps }) => (
+  <StyledHeadCell onClick={onClick} onTouchStart={() => ''} {...otherProps}>
     {children}
   </StyledHeadCell>
 );
 
 TableHeadCell.defaultProps = {
   onClick: () => {},
-  children: null,
-  className: '',
-  style: {}
+  children: null
 };
 
 TableHeadCell.propTypes = {
   onClick: propTypes.func,
-  children: propTypes.node,
-  className: propTypes.string,
-  style: propTypes.shape([propTypes.string, propTypes.number])
+  children: propTypes.node
 };
 
 export default TableHeadCell;
