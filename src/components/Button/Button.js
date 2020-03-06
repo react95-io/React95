5fe998fc2ef2f48b4a7a8eb51333afe789c7a906
Artur Bień <artur.bien@gmail.com>
2020-03-06 21:33:35 +0100

    Button forwardRef

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -99,70 +99,67 @@ export const StyledButton = styled.button`
   ${commonButtonStyles}
 `;
 
-const Button = ({
-  type,
-  onClick,
-  style,
-  disabled,
-  fullWidth,
-  size,
-  square,
-  active,
-  primary,
-  variant,
-  className,
-  children,
-  ...otherProps
-}) => (
-  <StyledButton
-    type={type}
-    variant={variant}
-    primary={primary}
-    onClick={disabled ? undefined : onClick}
-    style={style}
-    disabled={disabled}
-    isDisabled={disabled}
-    fullWidth={fullWidth}
-    size={size}
-    square={square}
-    active={active}
-    className={className}
-    // onTouchStart below to enable button :active style on iOS
-    onTouchStart={() => ''}
-    {...otherProps}
-  >
-    {children}
-  </StyledButton>
-);
+const Button = React.forwardRef(function Button(props, ref) {
+  const {
+    type,
+    onClick,
+    disabled,
+    fullWidth,
+    size,
+    square,
+    active,
+    primary,
+    variant,
+    children,
+    ...otherProps
+  } = props;
+
+  return (
+    <StyledButton
+      type={type}
+      variant={variant}
+      primary={primary}
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
+      isDisabled={disabled}
+      fullWidth={fullWidth}
+      size={size}
+      square={square}
+      active={active}
+      // onTouchStart below to enable button :active style on iOS
+      onTouchStart={() => ''}
+      ref={ref}
+      {...otherProps}
+    >
+      {children}
+    </StyledButton>
+  );
+});
 
 Button.defaultProps = {
   type: 'button',
   onClick: null,
-  style: {},
   disabled: false,
   fullWidth: false,
   size: 'md',
   square: false,
   active: false,
   primary: false,
-  variant: 'default',
-  className: ''
+  variant: 'default'
 };
 
 Button.propTypes = {
   type: propTypes.string,
   onClick: propTypes.func,
-  style: propTypes.shape([propTypes.string, propTypes.number]),
   disabled: propTypes.bool,
   fullWidth: propTypes.bool,
   size: propTypes.oneOf(['sm', 'md', 'lg']),
   square: propTypes.bool,
   active: propTypes.bool,
   primary: propTypes.bool,
-
   variant: propTypes.oneOf(['default', 'menu', 'flat']),
-  className: propTypes.string,
-  children: propTypes.node.isRequired
+  // eslint-disable-next-line react/require-default-props
+  children: propTypes.node
 };
 
 export default Button;
