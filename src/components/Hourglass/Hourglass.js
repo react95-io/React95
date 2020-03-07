851d6d848716cf6918762fc013ad2eb2d83e48cf
Artur Bień <artur.bien@gmail.com>
2020-03-07 09:49:00 +0100

    HourGlass forwardRef

--- src/components/Hourglass/Hourglass.js
+++ src/components/Hourglass/Hourglass.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import propTypes from 'prop-types';
-
 import styled from 'styled-components';
-
 import base64hourglass from './base64hourglass';
 
 const StyledContainer = styled.span`
@@ -16,29 +14,32 @@ const StyledHourglass = styled.span`
   width: 100%;
   height: 100%;
 `;
-const Hourglass = ({ size, className, style, ...otherProps }) => (
-  <StyledContainer
-    className={className}
-    style={{
-      ...style,
-      width: size || '30px',
-      height: size || '30px'
-    }}
-    {...otherProps}
-  >
-    <StyledHourglass />
-  </StyledContainer>
-);
+
+const Hourglass = React.forwardRef(function HourGlass(props, ref) {
+  const { size, style, ...otherProps } = props;
+  return (
+    <StyledContainer
+      style={{
+        ...style,
+        width: size || '30px',
+        height: size || '30px'
+      }}
+      ref={ref}
+      {...otherProps}
+    >
+      <StyledHourglass />
+    </StyledContainer>
+  );
+});
 
 Hourglass.defaultProps = {
   size: '30px',
-  className: '',
   style: {}
 };
 
 Hourglass.propTypes = {
   size: propTypes.oneOfType([propTypes.string, propTypes.number]),
-  className: propTypes.string,
-  style: propTypes.shape([propTypes.string, propTypes.number])
+  // eslint-disable-next-line react/forbid-prop-types
+  style: propTypes.object
 };
 export default Hourglass;
