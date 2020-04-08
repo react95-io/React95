876c17dee2f60fa73522a473e6623fc0188a4738
Artur Bień <artur.bien@gmail.com>
2020-04-08 21:14:19 +0200

    docs(globalstyle): add MS Sans Serif font

--- .storybook/decorators/globalStyle.js
+++ .storybook/decorators/globalStyle.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 
 import styleReset from '../../src/components/common/styleReset';
+import MSSansSerif from '../../src/components/assets/fonts/MS-Sans-Serif.woff2';
 
 const GlobalStyle = createGlobalStyle`
   ${styleReset}
+  @font-face {
+    font-family: 'MS-Sans-Serif';
+    src:  url('${MSSansSerif}') format('woff2');
+  }
+  body {
+    font-family: 'MS-Sans-Serif', 'sans-serif';
+  }
 `;
 
 export default storyFn => (
