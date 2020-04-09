cd2bbc6365d903d0cd45eaa6e85f3f4bf8d2ff0e
Artur Bień <artur.bien@gmail.com>
2020-04-09 15:40:38 +0200

    feat(avatar): add 'size' prop

--- src/components/Avatar/Avatar.stories.js
+++ src/components/Avatar/Avatar.stories.js
@@ -1,74 +1,36 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-
+import styled from 'styled-components';
 import Avatar from './Avatar';
-import AppBar from '../AppBar/AppBar';
-import Toolbar from '../Toolbar/Toolbar';
-import Button from '../Button/Button';
+
+const Wrapper = styled.div`
+  padding: 5rem;
+  background: ${({ theme }) => theme.material};
+  & > div > * {
+    margin-right: 1rem;
+  }
+`;
 
 storiesOf('Avatar', module)
-  .addDecorator(story => (
-    <div
-      style={{
-        padding: '5rem',
-        background: 'teal'
-      }}
-    >
-      {story()}
-    </div>
-  ))
+  .addDecorator(story => <Wrapper>{story()}</Wrapper>)
   .add('default', () => (
-    <AppBar>
-      <Toolbar
-        style={{ justifyContent: 'space-between', paddingRight: '1rem' }}
-      >
-        <Button flat accent>
-          Menu
-        </Button>
-        <Avatar src='https://sphoto.nasza-klasa.pl/33278012/1/square/2658174fbd.jpeg?v=1' />
-      </Toolbar>
-    </AppBar>
-  ))
-  .add('noBorder', () => (
-    <AppBar>
-      <Toolbar
-        style={{ justifyContent: 'space-between', paddingRight: '1rem' }}
-      >
-        <Button flat accent>
-          Menu
-        </Button>
-        <Avatar
-          src='https://sphoto.nasza-klasa.pl/33278012/1/square/2658174fbd.jpeg?v=1'
-          noBorder
-        />
-      </Toolbar>
-    </AppBar>
-  ))
-  .add('letter', () => (
-    <AppBar>
-      <Toolbar
-        style={{ justifyContent: 'space-between', paddingRight: '1rem' }}
-      >
-        <Button flat accent>
-          Menu
-        </Button>
-        <Avatar style={{ background: 'palevioletred' }}>AK</Avatar>
-      </Toolbar>
-    </AppBar>
-  ))
-  .add('square', () => (
-    <AppBar>
-      <Toolbar
-        style={{ justifyContent: 'space-between', paddingRight: '1rem' }}
-      >
-        <Button flat accent>
-          Menu
-        </Button>
-        <Avatar square>
-          <span role='img' aria-label='🚀'>
-            🚀
-          </span>
-        </Avatar>
-      </Toolbar>
-    </AppBar>
+    <div style={{ display: 'inline-flex' }}>
+      <Avatar
+        size={50}
+        src='https://sphoto.nasza-klasa.pl/33278012/1/square/2658174fbd.jpeg?v=1'
+      />
+      <Avatar
+        noBorder
+        size={50}
+        src='https://sphoto.nasza-klasa.pl/33278012/1/square/2658174fbd.jpeg?v=1'
+      />
+      <Avatar size={50} style={{ background: 'palevioletred' }}>
+        AK
+      </Avatar>
+      <Avatar square size={50}>
+        <span role='img' aria-label='🚀'>
+          🚀
+        </span>
+      </Avatar>
+    </div>
   ));
