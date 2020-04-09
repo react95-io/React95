ffc00530234389556785e75c5b3fe795787342b3
Artur Bień <artur.bien@gmail.com>
2020-04-09 16:11:14 +0200

    docs(colorinput stories): simplify stories

--- src/components/ColorInput/ColorInput.stories.js
+++ src/components/ColorInput/ColorInput.stories.js
@@ -8,25 +8,39 @@ import { ColorInput, Cutout } from '..';
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.material};
   padding: 5rem;
+  & > span {
+    margin-left: 1rem;
+    margin-right: 0.5rem;
+  }
 `;
 const StyledCutout = styled(Cutout)`
   background: ${({ theme }) => theme.canvas};
   display: inline-block;
-  padding: 4rem;
+  padding: 1rem;
+  & > span {
+    margin-left: 1rem;
+    margin-right: 0.5rem;
+  }
 `;
 storiesOf('ColorInput', module)
   .addDecorator(story => <Wrapper>{story()}</Wrapper>)
   .add('default', () =>
-    React.createElement(() => <ColorInput defaultValue='#00f' />)
-  )
-
-  .add('disabled', () =>
-    React.createElement(() => <ColorInput disabled defaultValue='#00f' />)
+    React.createElement(() => (
+      <>
+        <span>enabled: </span>
+        <ColorInput defaultValue='#00f' />
+        <span>disabled: </span>
+        <ColorInput disabled defaultValue='#00f' />
+      </>
+    ))
   )
   .add('flat', () =>
     React.createElement(() => (
       <StyledCutout>
+        <span>enabled: </span>
         <ColorInput variant='flat' defaultValue='#00f' />
+        <span>disabled: </span>
+        <ColorInput variant='flat' disabled defaultValue='#00f' />
       </StyledCutout>
     ))
   );
