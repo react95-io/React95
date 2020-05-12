2b0662b03cef29938fde9bb1174525b155a367b1
Artur Bień <artur.bien@gmail.com>
2020-05-12 20:39:04 +0200

    refactor(website): remove website from repo

--- website/src/components/Center.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-  display: inline-block;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
