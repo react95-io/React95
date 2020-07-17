f08c642bae72ac254a3c178a0e87dd165cfa2d83
Artur Bień <artur.bien@gmail.com>
2020-07-17 16:32:27 +0200

    feat(ms-sans-serif-bold): add bold font

--- .storybook/decorators/globalStyle.js
+++ .storybook/decorators/globalStyle.js
@@ -3,12 +3,21 @@ import { createGlobalStyle } from 'styled-components';
 
 import styleReset from '../../src/common/styleReset';
 import MSSansSerif from '../../src/assets/fonts/MS-Sans-Serif.woff2';
+import MSSansSerifBold from '../../src/assets/fonts/MS-Sans-Serif-Bold.woff2';
 
 const GlobalStyle = createGlobalStyle`
   ${styleReset}
   @font-face {
     font-family: 'MS-Sans-Serif';
-    src:  url('${MSSansSerif}') format('woff2');
+    src: url('${MSSansSerif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'MS-Sans-Serif';
+    src: url('${MSSansSerifBold}') format("woff2");
+    font-weight: bold;
+    font-style: normal
   }
   body {
     font-family: 'MS-Sans-Serif', 'sans-serif';
