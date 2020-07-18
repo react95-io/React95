0921725b41d1fd31fab326f55a33ea447c9c6b87
Artur Bień <artur.bien@gmail.com>
2020-07-19 00:13:57 +0200

    remove website from repo

--- website/src/components/Code.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default styled.p`
-  display: inline-block;
-  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-    monospace;
-  font-size: 1.4em;
-  white-space: nowrap;
-`;
