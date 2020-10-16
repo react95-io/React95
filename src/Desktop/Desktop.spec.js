2ef20b4caacf1e12833e556439d9ba42d86a73e0
Artur Bień <artur.bien@gmail.com>
2020-10-16 21:02:05 +0200

    feat(desktop): add Desktop component

---
+++ src/Desktop/Desktop.spec.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { renderWithTheme } from '../../test/utils';
+
+import Desktop from './Desktop';
+
+describe('<Desktop />', () => {
+  it('should render', () => {
+    const { container } = renderWithTheme(<Desktop />);
+    const desktopElement = container.firstChild;
+
+    expect(desktopElement).toBeInTheDocument();
+  });
+
+  it('should handle custom props', () => {
+    const customProps = { title: 'potatoe' };
+    const { container } = renderWithTheme(<Desktop {...customProps} />);
+    const desktopElement = container.firstChild;
+
+    expect(desktopElement).toHaveAttribute('title', 'potatoe');
+  });
+
+  describe('prop: backgroundStyles', () => {
+    it('should forward styles to background element', () => {
+      const { getByTestId } = renderWithTheme(
+        <Desktop backgroundStyles={{ backgroundColor: 'papayawhip' }} />
+      );
+      const backgroundElement = getByTestId('background');
+
+      expect(backgroundElement).toHaveAttribute(
+        'style',
+        'background-color: papayawhip;'
+      );
+    });
+  });
+
+  describe('prop: children', () => {
+    it('children should be rendered in background element', () => {
+      const { getByTestId } = renderWithTheme(<Desktop>Hi!</Desktop>);
+      const backgroundElement = getByTestId('background');
+
+      expect(backgroundElement.innerHTML).toBe('Hi!');
+    });
+  });
+});
