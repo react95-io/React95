ac91403ac8791d844b04b20c07e9dc8f0cfd7183
c33k <rltoscano@gmail.com>
2021-03-22 23:07:19 +0100

    refactor(datepicker): export datepicker

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ export { default as Checkbox } from './Checkbox/Checkbox';
 export { default as ColorInput } from './ColorInput/ColorInput';
 export { default as Counter } from './Counter/Counter';
 export { default as Cutout } from './Cutout/Cutout';
+export { default as DatePicker } from './DatePicker/DatePicker';
 export { default as Desktop } from './Desktop/Desktop';
 export { default as Divider } from './Divider/Divider';
 export { default as Fieldset } from './Fieldset/Fieldset';
