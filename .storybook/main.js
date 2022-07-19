f2c10fbe769a1203bc958641c1534a15a5df310c
Wes Souza <hey@wes.dev>
2022-07-18 20:37:47 -0400

    chore: fix Storybook deprecation warnings
    - Globs needed to be fixed
    - Implicit PostCSS was deprecated, so we need to explicitly tell Storybook we don't need it so the warning isn't shown

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.(js|mdx)',
-    '../.storybook/**/*.stories.(js|mdx)'
-  ],
+  stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
     {
       name: '@storybook/addon-docs',
@@ -14,5 +11,8 @@ module.exports = {
     },
     '@storybook/addon-storysource',
     'storybook-addon-styled-component-theme/dist/register'
-  ]
+  ],
+  features: {
+    postcss: false
+  }
 };
