0741a5841ac3c0b0fe1fb8c43cbca2b0e0cc69d2
Wes Souza <hey@wes.dev>
2022-07-25 17:01:44 -0400

    feat(hourglass): convert to TypeScript and export types

--- src/Hourglass/Hourglass.stories.tsx
+++ src/Hourglass/Hourglass.stories.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import styled from 'styled-components';
 
 import { Hourglass } from 'react95';
+import { ComponentMeta } from '@storybook/react';
 
 const Wrapper = styled.div`
   padding: 5rem;
@@ -12,7 +12,7 @@ export default {
   title: 'Hourglass',
   component: Hourglass,
   decorators: [story => <Wrapper>{story()}</Wrapper>]
-};
+} as ComponentMeta<typeof Hourglass>;
 
 export function Default() {
   return <Hourglass size={32} />;
