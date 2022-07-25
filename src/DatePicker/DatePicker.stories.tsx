c179bdcdbd0fd0a69db3d8f2e77c5276e0eebd9b
Wes Souza <hey@wes.dev>
2022-07-25 16:42:04 -0400

    feat(datepicker): convert to TypeScript and export types

---
+++ src/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase, react/jsx-pascal-case */
+import { ComponentMeta } from '@storybook/react';
+import { DatePicker__UNSTABLE } from 'react95';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 5rem;
+  background: ${({ theme }) => theme.desktopBackground};
+`;
+
+export default {
+  title: 'DatePicker__UNSTABLE',
+  component: DatePicker__UNSTABLE,
+  decorators: [story => <Wrapper>{story()}</Wrapper>]
+} as ComponentMeta<typeof DatePicker__UNSTABLE>;
+
+export function Default() {
+  return <DatePicker__UNSTABLE onAccept={date => console.log(date)} />;
+}
+
+Default.story = {
+  name: 'default'
+};
