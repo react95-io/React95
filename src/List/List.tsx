d99271ae408edc89c8894c4fca034f93b2cb39da
Wes Souza <hey@wes.dev>
2022-07-29 09:15:47 -0400

    fixes

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import styled from 'styled-components';
+
 import { createBorderStyles, createBoxStyles } from '../common';
 import { CommonStyledProps } from '../types';
 
--- src/ListItem/ListItem.tsx
+++ src/ListItem/ListItem.tsx
@@ -1,5 +1,4 @@
 import React, { forwardRef } from 'react';
-
 import styled from 'styled-components';
 import { createDisabledTextStyles } from '../common';
 import { blockSizes } from '../common/system';
